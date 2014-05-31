beb31c7d09736dbe190baef41e3543f4a5f230f8
wellziy <997132765@qq.com>
2014-05-31 19:16:08 +0800

    add httpcore to libs folder, finished Network class

--- src/topicfriendserver/network/Network.java
+++ src/topicfriendserver/network/Network.java
@@ -1,9 +1,11 @@
 package topicfriendserver.network;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,9 +14,11 @@ import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.Vector;
 
+import org.apache.http.util.ByteArrayBuffer;
+
 public class Network 
 {
-	public static int NULL_CONNECTION=0;
+	public static final int NULL_CONNECTION=0;
 	
 	//store all client connections after handshake in server
 	//store all connection call from connectHostPort
@@ -23,18 +27,27 @@ public class Network
 	private static ArrayList<Integer> s_connections;
 	private static HashSet<Integer> s_waitingConnections;
 	private static int s_waitingConectionsMaxSize;
+	private static HashSet<Integer> s_badConnections;
 	
 	//only for server
 	private static HashSet<Socket> s_handshakeSockets;
 	private static int s_handshakeSocketsMaxSize;
 	private static ServerSocket s_serverSocket;
+	private static Thread s_acceptThread;
+	private static boolean s_isServerActive;
 	
 	//public interfaces
 	public static void initNetwork(int minThreadCount,int maxThreadCount,int waitingConectionsMaxSiz)
 	{
 		NetworkWorkerPool.initNetworkWorkerPool(minThreadCount, maxThreadCount);
+		s_waitingConnections=new HashSet<Integer>();
 		s_waitingConectionsMaxSize=waitingConectionsMaxSiz;
+		
+		s_connectionSocketMap=new HashMap<Integer,Socket>();
 		s_lastConnectionID=NULL_CONNECTION;
+		
+		s_badConnections=new HashSet<Integer>();
+		
 		s_serverSocket=null;
 	}
 	
@@ -46,56 +59,173 @@ public class Network
 		s_handshakeSocketsMaxSize=handshakeSocketsMaxSize;
 		
 		s_serverSocket=new ServerSocket();
+		s_serverSocket.setSoTimeout(1);
 		s_serverSocket.bind(new InetSocketAddress(port));
 		
+		s_acceptThread=new Thread(new Runnable()
+		{
+			@Override
+			public void run() 
+			{
+				while(true)
+				{
+					try
+					{
+						Socket socket=s_serverSocket.accept();
+						synchronized (s_handshakeSockets)
+						{
+							if(s_handshakeSockets.size()>=s_handshakeSocketsMaxSize)
+							{
+								closeSocket(socket);
+							}
+							else
+							{
+								s_handshakeSockets.add(socket);
+							}
+						}
+					} 
+					catch (SocketTimeoutException e)
+					{
+						//accept timeout,nothing to do here
+					}
+					catch (IOException e) 
+					{
+						e.printStackTrace();
+						assert(false);
+					}
+					
+					//check whether network is active or not
+					if(!s_isServerActive)
+					{
+						break;
+					}
+				}
+			}
+		});
+		//must set s_isServerActive to true before start the thread
+		s_isServerActive=true;
+		s_acceptThread.start();
 	}
 	
 	public static void stopServer()
 	{
+		assert(isServerRunning());
+		//must set s_isServerActive to false before stop the thread
+		s_isServerActive=false;
+		//stop the thread,must stop the thread before set s_serverSocket to null
+		try
+		{
+			s_acceptThread.join();
+		} 
+		catch (InterruptedException e) 
+		{
+			e.printStackTrace();
+		}
 		
+		s_handshakeSockets.clear();
+		try 
+		{
+			s_serverSocket.close();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		s_serverSocket=null;
 	}
 	
 	public static void destroyNetwork()
 	{
+		if(isServerRunning())
+		{
+			System.out.println("must stop server before");
+			assert(false);
+		}
 		
+		Iterator<Integer> iter=s_connections.iterator();
+		while(iter.hasNext())
+		{
+			int connection=iter.next();
+			disconnectWithoutChangeConnection(connection);
+		}
+		s_connections.clear();
+		
+		NetworkWorkerPool.destroyNetworkWorkerPool();
+		s_connectionSocketMap.clear();
+		s_waitingConnections.clear();
+		s_badConnections.clear();
 	}
 	
 	public static int getConnectionCount()
 	{
-		//TODO:
-		return 0;
+		return s_connections.size();
 	}
 	
 	public static boolean isServerRunning()
 	{
-		//TODO:
-		return false;
+		return s_serverSocket!=null&&s_isServerActive==true;
 	}
 	
-	public static int connectHostPort(String host,int port)
+	public static int connectHostPort(String host,int port,int timeout) throws IOException
 	{
-		//TOOD:
-		return 0;
+		InetSocketAddress address=new InetSocketAddress(host, port);
+		Socket socket=new Socket();
+		socket.connect(address, timeout);
+		
+		OutputStream outputStream = socket.getOutputStream();
+		byte[] handshakeByte=new byte[4];
+		outputStream.write(handshakeByte);
+		outputStream.flush();
+		
+		int connection=createConnection(socket);
+		if(s_waitingConnections.size()>=s_waitingConectionsMaxSize)
+		{
+			closeSocket(socket);
+			return NULL_CONNECTION;
+		}
+		
+		//add to waiting connection
+		s_waitingConnections.add(connection);
+		//add to connections
+		s_connections.add(connection);
+		
+		return connection;
 	}
 	
 	public static int acceptConnection()
 	{
-		//TODO:
-		return 0;
+		assert(isServerRunning());
+		
+		int connection = acceptHankshakeSocket();
+		
+		return connection;
 	}
 	
+	//disconnect a connection,remove all thing about it
 	public static boolean disconnect(int connection)
 	{
-		//TODO:
-		return false;
+		//remove from connections
+		Iterator<Integer> iter = s_connections.iterator();
+		while(iter.hasNext())
+		{
+			int iterConnection=iter.next();
+			if(iterConnection==connection)
+			{
+				disconnectWithoutChangeConnection(iterConnection);
+				iter.remove();
+				break;
+			}
+		}
+		return true;
 	}
 	
 	
 	//may be change to get all bad connections at once
-	public static int getBadConnection()
+	public static HashSet<Integer> getBadConnectionSet()
 	{
-		//TODO:
-		return 0;
+		updateBadConnectionSet();
+		
+		return s_badConnections;
 	}
 	
 	public static void makeBadConnection(int connection)
@@ -103,25 +233,103 @@ public class Network
 		
 	}
 	
-	public static int receiveData(ByteBuffer buf)
+	//recvConnection == NULL_CONNECTION means receive the first socket which has data received
+	public static int receiveData(ByteArrayBuffer buf,int recvConnection)
 	{
-		//TODO:
-		return 0;
+		//check wating queue
+		Iterator<Integer> iter = s_waitingConnections.iterator();
+		while(iter.hasNext())
+		{
+			int connection=iter.next();
+			Socket socket=getSocketByConnection(connection);
+			try 
+			{
+				if(socket.getInputStream().available()>=4)
+				{
+					iter.remove();
+					NetworkWorkerPool.queueRecieveData(socket);
+					continue;
+				}
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+				//add to the bad connection set
+				s_badConnections.add(connection);
+				iter.remove();
+			}
+		}
+		
+		Socket recvSocket=null;
+		//find the socket with the connection
+		if(recvConnection!=NULL_CONNECTION)
+		{
+			recvSocket=getSocketByConnection(recvConnection);
+		}
+		
+		//receive data from the socket,the socket may be null here
+		recvSocket=NetworkWorkerPool.getReceivedData(buf, recvSocket);
+		//if no socket has data to received
+		if(recvSocket==null)
+		{
+			return NULL_CONNECTION;
+		}
+		
+		if(s_waitingConnections.size()>=s_waitingConectionsMaxSize)
+		{
+			//if the waiting set is full,just close the socket
+			closeSocket(recvSocket);
+			return NULL_CONNECTION;
+		}
+		
+		if(recvConnection==NULL_CONNECTION)
+		{
+			recvConnection=getConnectionBySocket(recvSocket);
+		}
+		assert(recvConnection!=NULL_CONNECTION);
+		//add the connection back to waiting queue
+		s_waitingConnections.add(recvConnection);
+		
+		return recvConnection;
 	}
 	
-	public static void sendData(ByteBuffer buf,int connection)
+	public static void sendDataOne(ByteArrayBuffer buf,int connection)
 	{
+		Socket socket=getSocketByConnection(connection);
+		assert(socket!=null);
 		
+		updateBadConnectionSet();
+		if(s_badConnections.contains(connection))
+		{
+			System.out.println("send data to a bad connection, will ignore it");
+			return;
+		}
+		
+		NetworkWorkerPool.queueData(socket, buf);
 	}
 	
-	public static void sendDataAllExcept(ByteBuffer buf,int connection)
+	public static void sendDataAllExcept(ByteArrayBuffer buf,int connection)
 	{
-		
+		Iterator<Integer> iter = s_connections.iterator();
+		while(iter.hasNext())
+		{
+			int iterConnection=iter.next();
+			if(iterConnection==connection)
+			{
+				continue;
+			}
+			sendDataOne(buf, iterConnection);
+		}
 	}
 	
-	public static void sendDataMany(ByteBuffer buf,Vector<Integer> connections)
+	public static void sendDataMany(ByteArrayBuffer buf,Vector<Integer> connections)
 	{
-		
+		Iterator<Integer> iter = connections.iterator();
+		while(iter.hasNext())
+		{
+			int iterConnection=iter.next();
+			sendDataOne(buf, iterConnection);
+		}
 	}
 	
 	//private methods
@@ -174,28 +382,31 @@ public class Network
 	{
 		Socket socket=null;
 		
-		//find the socket ready to read
-		Iterator<Socket>iter=s_handshakeSockets.iterator();
-		while(iter.hasNext())
+		synchronized (s_handshakeSockets) 
 		{
-			Socket iterSocket=iter.next();
-			try
+			//find the socket ready to read
+			Iterator<Socket>iter=s_handshakeSockets.iterator();
+			while(iter.hasNext())
 			{
-				if(iterSocket.getInputStream().available()>=4)
+				Socket iterSocket=iter.next();
+				try
 				{
-					socket=iterSocket;
+					if(iterSocket.getInputStream().available()>=4)
+					{
+						socket=iterSocket;
+						iter.remove();
+						break;
+					}
+				} 
+				catch (IOException e)
+				{
+					e.printStackTrace();
+					closeSocket(iterSocket);
 					iter.remove();
-					break;
 				}
-			} 
-			catch (IOException e)
-			{
-				e.printStackTrace();
-				closeSocket(iterSocket);
-				iter.remove();
 			}
 		}
-		
+
 		if(socket==null)
 		{
 			return NULL_CONNECTION;
@@ -236,6 +447,41 @@ public class Network
 		catch (IOException e)
 		{
 			e.printStackTrace();
+		}
+	}
+	
+	private static void disconnectWithoutChangeConnection(int connection)
+	{
+		//remove from waiting queue
+		removeWaitingConnection(connection);
+		//remove from bad connection
+		s_badConnections.remove(connection);
+		
+		//close the socket
+		Socket socket=getSocketByConnection(connection);
+		closeSocket(socket);
+		//remove from connection and socket map
+		s_connectionSocketMap.remove(connection);
+		
+		//remove from network worker pool
+		NetworkWorkerPool.removePendingReceiveSocket(socket);
+		NetworkWorkerPool.removeBadSocket(socket);
+	}
+	
+	private static void updateBadConnectionSet()
+	{
+		HashSet<Socket> badSocketSet = NetworkWorkerPool.getBadSocketSet();
+		synchronized (badSocketSet)
+		{
+			Iterator<Socket> iter = badSocketSet.iterator();
+			while(iter.hasNext())
+			{
+				Socket socket=iter.next();
+				int connection=getConnectionBySocket(socket);
+				assert(connection!=NULL_CONNECTION);
+				s_badConnections.add(connection);
+			}
+			badSocketSet.clear();
 		}
 	}
 }
--- src/topicfriendserver/network/NetworkWorkerPool.java
+++ src/topicfriendserver/network/NetworkWorkerPool.java
@@ -1,10 +1,35 @@
 package topicfriendserver.network;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import org.apache.http.util.ByteArrayBuffer;
+
+class NetworkBuffer
+{
+	public Socket socket;
+	public ByteArrayBuffer content;
+}
 
 public class NetworkWorkerPool
 {
+	public static final int MAX_DATA_SIZE=1000000;
+	
+	private static HashSet<Socket> s_receiveSockets;
+	private static HashSet<Socket> s_badSockets;
+	private static ArrayList<NetworkBuffer> s_sendBuffers;
+	private static ArrayList<NetworkBuffer> s_receiveBuffers;
+	private static ArrayList<Thread> s_threads;
+	private static Integer s_waitingThreadCount;
+	private static int s_minThreadCount;
+	private static int s_maxThreadCount;
+	private static Boolean s_isActive;
+	
+	//public interfaces
 	public static void initNetworkWorkerPool(int minThreadCount,int maxThreadCount)
 	{
 		
@@ -20,13 +45,13 @@ public class NetworkWorkerPool
 		
 	}
 	
-	public static Socket getReceivedData(ByteBuffer buf,Socket socket)
+	public static Socket getReceivedData(ByteArrayBuffer buf,Socket socket)
 	{
 		//TODO:
 		return null;
 	}
 	
-	public static void queueData(Socket socket,ByteBuffer buf)
+	public static void queueData(Socket socket,ByteArrayBuffer buf)
 	{
 		
 	}
@@ -41,14 +66,124 @@ public class NetworkWorkerPool
 		
 	}
 	
-	public static Socket getBadSocket()
+	public static HashSet<Socket> getBadSocketSet()
 	{
 		//TODO:
 		return null;
 	}
 	
-	public static void makeBadSocket(Socket socket)
+	//private methods
+	private static int packInt(byte[] networkByte)
 	{
+		//TODO:
+		return 0;
+	}
+	
+	private static byte[] unpackInt(int value)
+	{
+		//TODO:
+		return null;
+	}
+	
+	private static boolean receiveWithTimeout(Socket socket,ByteArrayBuffer buf,int len,int eachTimeout,int totalTimeout)
+	{
+		long totalCounter=0;
 		
+		InputStream inputStream=null;
+		try
+		{
+			inputStream = socket.getInputStream();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+			return false;
+		}
+		
+		byte[] byteBuffer=new byte[1024];
+		while(len>0)
+		{
+			long startLoopTime=System.currentTimeMillis();
+			try 
+			{
+				while(inputStream.available()<=0)
+				{
+					Thread.sleep(1);
+					long diff=System.currentTimeMillis()-startLoopTime;
+					if(diff>=eachTimeout)
+					{
+						return false;
+					}
+				}
+				totalCounter+=System.currentTimeMillis()-startLoopTime;
+				if(totalCounter>=totalTimeout)
+				{
+					return false;
+				}
+				
+				int readLen=inputStream.read(byteBuffer,0,byteBuffer.length);
+				buf.append(byteBuffer, 0, readLen);
+				len-=readLen;
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+				return false;
+			}
+			catch (InterruptedException e) 
+			{
+				e.printStackTrace();
+				return false;
+			}
+			
+		}
+		return true;
+	}
+	
+	private static NetworkBuffer receiveNetworkBuffer(Socket socket)
+	{
+		ByteArrayBuffer header=new ByteArrayBuffer(8);
+		
+		boolean isHeaderOK=receiveWithTimeout(socket, header, 4, 3000, 30000);
+		if(!isHeaderOK)
+		{
+			return null;
+		}
+		
+		int size=packInt(header.toByteArray());
+		ByteArrayBuffer content=new ByteArrayBuffer(size+100);
+		boolean isContentOK=receiveWithTimeout(socket, content, size, 3000, 30000);
+		if(!isContentOK)
+		{
+			return null;
+		}
+		
+		NetworkBuffer res=new NetworkBuffer();
+		res.socket=socket;
+		res.content=content;
+		
+		return res;
+	}
+	
+	private static boolean sendNetworkBuffer(NetworkBuffer buf)
+	{
+		Socket socket=buf.socket;
+		
+		try
+		{
+			OutputStream outputStream = socket.getOutputStream();
+			int len=buf.content.length();
+			byte[] header=unpackInt(len);
+			outputStream.write(header,0,4);
+			outputStream.write(buf.content.buffer());
+			outputStream.flush();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
 	}
 }
